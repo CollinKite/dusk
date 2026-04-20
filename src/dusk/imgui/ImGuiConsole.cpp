5dcbca392d42926cfcdffd302835207bd26ad489
madeline <qwertytrogi@gmail.com>
2026-04-20 09:51:33 -0700

    fix turbo key getting stuck on

--- src/dusk/imgui/ImGuiConsole.cpp
+++ src/dusk/imgui/ImGuiConsole.cpp
@@ -280,7 +280,8 @@ namespace dusk {
     }
 
     void ImGuiConsole::UpdateSettings() {
-        getTransientSettings().skipFrameRateLimit = getSettings().game.enableTurboKeybind && ImGui::IsKeyDown(ImGuiKey_Tab);
+        getTransientSettings().skipFrameRateLimit = getSettings().game.enableTurboKeybind &&
+            SDL_GetKeyboardState(nullptr)[SDL_SCANCODE_TAB];
 
         if (dusk::frame_interp::get_ui_tick_pending() && mDoMain::developmentMode == 1 && (mDoCPd_c::getHold(PAD_1) & (PAD_TRIGGER_R | PAD_TRIGGER_L)) == (PAD_TRIGGER_R | PAD_TRIGGER_L) && mDoCPd_c::getTrigY(PAD_1)) {
             getTransientSettings().moveLinkActive = !getTransientSettings().moveLinkActive;
